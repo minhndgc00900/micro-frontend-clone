9f9a5cdc894ee597d5b1bbfc10de2e9a755f4daf
Minh Nguyen <minhnd0496@gmail.com>
2022-08-16 18:19:33 +0700

    in progress

--- container/vite.config.ts
+++ container/vite.config.ts
@@ -1,7 +1,29 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import tsconfigPaths from 'vite-tsconfig-paths';
+import { createHtmlPlugin } from 'vite-plugin-html';
+import federation from "@originjs/vite-plugin-federation";
+
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()]
+  mode: 'development',
+  server: {
+    port: 3000,
+  },
+  plugins: [
+    react(),
+    tsconfigPaths(),
+    federation({
+      name: 'container',
+      remotes: {
+        marketing: 'http://127.0.0.1:3001/remoteEntry.js'
+      }
+    }),
+    createHtmlPlugin({
+      minify: true,
+      entry: 'src/index.ts',
+      template: 'public/index.html',
+    }),
+  ],
 })
